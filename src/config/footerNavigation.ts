03fa9d461bf340b1f8afc77ff1fe33077611a24a
CP <cprakashagr@gmail.com>
2024-10-14 17:31:28 +0530

    Initial commit for v2

---
+++ src/config/footerNavigation.ts
@@ -0,0 +1,127 @@
+// Footer Navigation
+// ------------
+// Description: The footer navigation data for the website.
+export interface Logo {
+	// src: string
+	alt: string
+	text: string
+}
+
+export interface FooterAbout {
+	title: string
+	aboutText: string
+	// logo: Logo
+}
+
+export interface SubCategory {
+	subCategory: string
+	subCategoryLink: string
+	nested: SubCategory[]
+}
+
+export interface FooterColumn {
+	category: string
+	subCategories: SubCategory[]
+}
+
+export interface SubFooter {
+	copywriteText: string
+}
+
+export interface FooterData {
+	footerAbout: FooterAbout
+	footerColumns: FooterColumn[]
+	subFooter: SubFooter
+}
+
+export const footerNavigationData: FooterData = {
+	footerAbout: {
+		title: 'globetrotter.finance',
+		aboutText:
+			'Empowering businesses with cutting-edge financial security, advanced digital analytics, and comprehensive CA and legal consultancy services. Your trusted partner in safeguarding identities, optimizing performance, and ensuring compliance.',
+		logo: {
+			text: 'globetrotter.finance'
+		}
+	},
+	footerColumns: [
+		{
+			category: 'financial.kuber',
+			subCategories: [
+				{
+					subCategory: 'identity frauds',
+					subCategoryLink: '/kuber',
+				},
+				{
+					subCategory: 'deduplication',
+					subCategoryLink: '/kuber',
+				},
+				{
+					subCategory: 'anti money laundering',
+					subCategoryLink: '/kuber',
+				},
+				// {
+				// 	subCategory: 'stock markets - nudges',
+				// 	subCategoryLink: '/pricing',
+				// },
+				// {
+				// 	subCategory: 'stock markets - analysis',
+				// 	subCategoryLink: '/pricing',
+				// },
+				// {
+				// 	subCategory: 'stock markets - automation',
+				// 	subCategoryLink: '/pricing',
+				// }
+			]
+		},
+		{
+			category: 'digital.mantarays',
+			subCategories: [
+				{
+					subCategory: 'leakage identification',
+					subCategoryLink: '/mantarays',
+				},
+				{
+					subCategory: 'optimization solutions',
+					subCategoryLink: '/mantarays',
+				}
+			]
+		},
+		{
+			category: 'legal.saras',
+			subCategories: [
+				{
+					subCategory: 'due diligence',
+					subCategoryLink: '/saras'
+				},
+				{
+					subCategory: 'contracts intelligence',
+					subCategoryLink: '/saras'
+				},
+				{
+					subCategory: 'legal excellence',
+					subCategoryLink: '/saras'
+				}
+			]
+		},
+		{
+			category: 'about.us',
+			subCategories: [
+				{
+					subCategory: 'privacy policy',
+					subCategoryLink: '/privacy'
+				},
+				{
+					subCategory: 'terms & conditions',
+					subCategoryLink: '/terms'
+				},
+				{
+					subCategory: 'contact',
+					subCategoryLink: '/contact'
+				}
+			]
+		}
+	],
+	subFooter: {
+		copywriteText: '© globetrotter.finance 2024.'
+	}
+}
---
+++ src/config/navigationBar.ts
@@ -0,0 +1,40 @@
+// Navigation Bar
+// ------------
+// Description: The navigation bar data for the website.
+export interface Logo {
+	src: string
+	alt: string
+	text: string
+}
+
+export interface NavSubItem {
+	name: string
+	link: string
+}
+
+export interface NavItem {
+	name: string
+	link: string
+	submenu?: NavSubItem[]
+}
+
+export interface NavData {
+	logo: Logo
+	navItems: NavItem[]
+}
+
+export const navigationBarData: NavData = {
+	logo: {
+		// src: '/logo.svg',
+		alt: 'globetrotter.finance',
+		text: 'globetrotter.finance'
+	},
+	navItems: [
+		{ name: 'home', link: '/' },
+		{ name: 'kuber', link: '/kuber' },
+		{ name: 'mantarays', link: '/mantarays' },
+		{ name: 'saras', link: '/saras' },
+		{ name: 'contact', link: '/contact' }
+	],
+	navActions: []
+}
